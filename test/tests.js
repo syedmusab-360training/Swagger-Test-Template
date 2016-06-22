58dc465fbf313410829c37f804c676607d0e7f70
syedmusab <syed.musab@360training.com>
2016-06-22 16:26:52 +0500

    Test

--- test/tests.js
+++ test/tests.js
@@ -26,7 +26,7 @@ describe('test generation with inference', function () {
               "uri": "localhost/v1/pets"
           },
           "response": {
-              "status": 400,
+              "status": 200,
               "headers": {
                   "content-type": "application/json"
               }
